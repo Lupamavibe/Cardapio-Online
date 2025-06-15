ca7aa1a5f0b55ee0bc68b666dd92d0cc2c78bba9
lupamavibe <lupamavibe2023@gmail.com>
2025-06-15 19:09:17 -0300

    Alterado o horario de abertura do restaurante

--- script.js
+++ script.js
@@ -183,7 +183,7 @@ checkoutBtn.addEventListener("click", function(){
 function checkRestauranteOpen(){
     const data  = new Date();
     const hora = data.getHours();
-    return hora >= 1 && hora < 13;
+    return hora >= 18 && hora < 24;
     //retorna TRUE para a condição
 }
 
