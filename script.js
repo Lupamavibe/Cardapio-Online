b0e1bc81cc225f1c1309997f8b89011d06c0fa7f
lupamavibe <lupamavibe2023@gmail.com>
2025-06-26 05:19:17 -0300

    Alterado o horario de funcionamento

--- script.js
+++ script.js
@@ -183,7 +183,7 @@ checkoutBtn.addEventListener("click", function(){
 function checkRestauranteOpen(){
     const data  = new Date();
     const hora = data.getHours();
-    return hora >= 18 && hora < 24;
+    return hora >= 5 && hora < 24;
     //retorna TRUE para a condição
 }
 
